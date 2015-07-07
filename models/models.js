1636f98ab672f3868e1f18e7a5618051520fb127
Fernando Simon <fernansimon@yahoo.es>
2015-07-07 12:08:29 +0200

    Base de datos

--- models/models.js
+++ models/models.js
@@ -29,6 +29,10 @@ var sequelize = new Sequelize (DB_name, user, pwd,
 );
 
 
+//Usar BBDD SQLite:
+//var sequelize = new Sequelize ( null, null, null,
+	//															{ dialect: "sqlite", storage: "quiz.sqlite"}
+		//														);
 
 //Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
-var quizController = require('../controllers/quiz_controller');
+var quizController = require('../controllers/quiz_controllers');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'quiz' });
+  res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes', quizController.index);
-router.get('/quizes/:QuizId(\\d+)', quizController.show);
-router.get('/quizes/:QuizId(\\d+)/answer', quizController.answer);
+// Autoload de comandos con :quizId
 
-router.get('/author', quizController.author);
+router.param('quizId', quizController.load); //autoload  :quizId
 
+
+//Antes de la BBDD
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer);
+
+//con BBDD
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+
 //router.get('/author', quizController.author);
 
 module.exports = router;
