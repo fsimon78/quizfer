470722f2e3bb618620fbaa563c866fe9f1f1598e
Fernando Simon <fernansimon@yahoo.es>
2015-07-13 12:01:06 +0200

    Edita y borra

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var quizController = require('../controllers/quiz_controllers');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors:[]});
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 // Autoload de comandos con :quizId
@@ -22,7 +22,12 @@ router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
-router.get('/author', quizController.author);
+
+
+//router.get('/author', quizController.author);
 
 module.exports = router;
