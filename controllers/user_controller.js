24e3f17bd20cb5a6c60b07e03494a1fb9e592eaa
Fernando Simon <fernansimon@yahoo.es>
2015-07-17 11:25:28 +0200

    Login

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,16 +1,13 @@
-var users = {
-		admin: {id:1, username:"admin", Password:"1234"},
-		pepe: {id:2, username:"pepe", Password:"5678"}
-		};
-		
-// comprueba si el usuario esta registrado en users
-// Si autentificación falla o hay errores se ejecuta callback(error)
+var users = { admin: {id:1, username:"admin", password:"1234"},
+              pepe: {id:2, username:"pepe", password:"5678"}
+            };
 
-exports.autenticar = function(login, Password, callback){
-		if(users[login]){
-			if (Password === users[login].Password){
-					callback(null, users[login]);
-				}
-			else { callback(new Error('Password erróneo.'));}
-		} else{ callback(new Error('No existe el usuario.'));}
+// Comprueba si el usuario esta registrado en users
+// Si autenticación falla o hay errores se ejecuta callback(error).
+exports.autenticar = function(login, password, callback) {
+  if (users[login]) {
+    if (password === users[login].password) {
+      callback(null, users[login]);
+    } else { callback(new Error('password erroneo.')); }
+  } else { callback(new Error('No existe el usuario.')); }
 };					
